71d45392ff1840ac719c1b5b2b6d8d663b96e316
Ryan Bigg <ryanbigg@cultureamp.com>
2016-11-21 08:37:53 +1100

    Bump to 2.0.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chronic.Mixfile do
 
   def project do
     [app: :chronic,
-     version: "2.0.0",
+     version: "2.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
