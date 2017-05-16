352a54de21a206ea4aac5560248862db2d2177c3
Venkat Dinavahi <vendiddy@gmail.com>
2017-05-16 17:57:51 -0400

    bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chronic.Mixfile do
 
   def project do
     [app: :chronic,
-     version: "2.0.1",
+     version: "2.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
