3547477e580b6fc954e2dad5f38f726c21fbcbc4
Ryan Bigg <me@ryanbigg.com>
2018-09-25 11:49:40 +1000

    Bump earmark + ex_doc dependencies

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chronic.Mixfile do
 
   def project do
     [app: :chronic,
-     version: "3.0.0",
+     version: "3.1.0",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -17,8 +17,8 @@ defmodule Chronic.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev},
+      {:earmark, "~> 1.2.6", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
    ]
   end
